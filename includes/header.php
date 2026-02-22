dc7dee11df792ead26e70518c8590c347522c51d
iooner <contact@iooner.be>
2026-02-22 11:46:08 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <header class="py-12 relative">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div class="flex items-center justify-center md:justify-start">
-            <a href="/" title="Retour à la page d'accueil" class="block">
-                <img src="images/logo.svg" alt="Liège Hackerspace - Learn \ Make \ Share" class="h-32 w-auto">
-            </a>
+        <a href="/" title="Retour à la page d'accueil" class="block">
+            <img src="images/logo.svg" alt="Liège Hackerspace - Learn \ Make \ Share" class="h-32 w-auto">
+        </a> 
         </div>
         <nav class="flex items-center justify-center md:justify-end mt-8 md:mt-0">
             <ul class="flex flex-wrap justify-center md:justify-end gap-2">
